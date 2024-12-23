4b1e50639cf45feff18c3613943a221afc446c17
Christopher Lenfant <lenfant.chris@hotmail.fr>
2024-12-23 21:25:57 +0100

    Add CoutingSort

---
+++ c#/DsaExercises/Arrays/CountingSort.cs
@@ -0,0 +1,45 @@
+namespace Arrays;
+
+public static class CountingSort
+{
+    public static int[] Sort(int[] arrayToSort)
+    {
+        int maxVal = GetMaxVal(arrayToSort);
+        
+        int[] countingArray = new int[maxVal + 1];
+
+        for (int i = 0; i < arrayToSort.Length; i++)
+        {
+            countingArray[arrayToSort[i]]++;
+        }
+        
+        Console.WriteLine("Counting array [{0}]", string.Join(", ", countingArray));
+
+        for (int i = 0, j = 0; i <= maxVal; i++)
+        {
+            while (countingArray[i] > 0)
+            {
+                arrayToSort[j] = i;
+                j++;
+                countingArray[i]--;
+            }
+        }
+        
+        return arrayToSort;
+    }
+
+    private static int GetMaxVal(int[] arrayToSort)
+    {
+        int maxVal = 0;
+
+        for (int i = 0; i < arrayToSort.Length; i++)
+        {
+            if(maxVal < arrayToSort[i])
+            {
+                maxVal = arrayToSort[i];
+            }
+        }
+
+        return maxVal;
+    }
+}
--- c#/DsaExercises/Arrays/Program.cs
+++ c#/DsaExercises/Arrays/Program.cs
@@ -2,7 +2,8 @@
 
 int[] array = [7, 12, 9, 11, 3, 15, 22, 1, 54, 9, 65, 19];
 
-var sortedArray = QuickSort.Sort(array, 0, array.Length - 1);
-
 Console.WriteLine("Array [{0}]", string.Join(", ", array));
+
+var sortedArray = CountingSort.Sort(array);
+
 Console.WriteLine("Sorted array : [{0}]", string.Join(", ", sortedArray));
