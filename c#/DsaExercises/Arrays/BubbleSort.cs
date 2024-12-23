31a24656278b2efd96b0ffe6541401067a7564e8
Christopher Lenfant <c.lenfant@betclicgroup.com>
2024-12-23 13:23:41 +0100

    Add optimized version of bubble sort

--- c#/DsaExercises/Arrays/BubbleSort.cs
+++ c#/DsaExercises/Arrays/BubbleSort.cs
@@ -6,6 +6,8 @@ public static class BubbleSort
     {
         for (int i = 0; i < arrayToSort.Length - 1; i++)
         {
+            var swapped = false;
+            
             for (int j = 0; j < arrayToSort.Length -1 - i; j++)
             {
                 if (arrayToSort[j] > arrayToSort[j + 1])
@@ -13,8 +15,13 @@ public static class BubbleSort
                     int temp = arrayToSort[j];
                     arrayToSort[j] = arrayToSort[j + 1];
                     arrayToSort[j + 1] = temp;
+                    
+                    swapped = true;
                 }
             }
+
+            if (!swapped)
+                break;
         }
 
         return arrayToSort;
