d5daf0b6ef17a5cc936749e7f22e68d91492b7ac
Christopher Lenfant <lenfant.chris@hotmail.fr>
2024-12-24 14:14:28 +0100

    Add MergeSort

---
+++ c#/DsaExercises/Arrays/MergeSort.cs
@@ -0,0 +1,57 @@
+namespace Arrays;
+
+public static class MergeSort
+{
+    public static int[] Sort(int[] arrayToSort)
+    {
+        if (arrayToSort.Length <= 1)
+        {
+            return arrayToSort;
+        }
+
+        int middle = arrayToSort.Length / 2;
+        
+        var leftArray = arrayToSort[..middle];
+        var rightArray = arrayToSort[middle..];
+
+        var sortedLeftArray = Sort(leftArray);
+        var sortedRightArray = Sort(rightArray);
+        
+        return Merge(sortedLeftArray, sortedRightArray);
+    }
+
+    private static int[] Merge(int[] leftArray, int[] rightArray)
+    {
+        int[] result = new int[leftArray.Length + rightArray.Length];
+        
+        var i = 0;
+        var j = 0;
+        var k = 0;
+        
+        while (i < leftArray.Length && j < rightArray.Length)
+        {
+            if (leftArray[i] <= rightArray[j])
+            {
+              result[k++] = leftArray[i];
+              i++;
+            }
+            else
+            {
+                result[k++] = rightArray[j];
+                j++;
+            }
+        }
+        
+        while (i < leftArray.Length)
+        {
+            result[k++] = leftArray[i++];
+        }
+        
+        while (j < rightArray.Length)
+        {
+            result[k++] = rightArray[j++];
+        }
+        
+        return result;
+    }
+}
--- c#/DsaExercises/Arrays/Program.cs
+++ c#/DsaExercises/Arrays/Program.cs
@@ -4,6 +4,6 @@ int[] array = [7, 12, 9, 11, 3, 15, 22, 1, 54, 9, 65, 19];
 
 Console.WriteLine("Array [{0}]", string.Join(", ", array));
 
-var sortedArray = RadixSort.Sort(array);
+var sortedArray = MergeSort.Sort(array);
 
 Console.WriteLine("Sorted array : [{0}]", string.Join(", ", sortedArray));
